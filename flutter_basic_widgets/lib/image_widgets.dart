0b7341a75a1d7b83598facc0c17b2e177067dbab
Arda Sak <ardasak434@gmail.com>
2022-02-01 00:05:49 +0300

    FadeinImage, IntrinsicHeight and Placeholder

--- flutter_basic_widgets/lib/image_widgets.dart
+++ flutter_basic_widgets/lib/image_widgets.dart
@@ -2,34 +2,55 @@ import 'package:flutter/material.dart';
 
 class ImageWidget extends StatelessWidget {
   const ImageWidget({Key? key}) : super(key: key);
-  final _imgUrl =
-      "https://i.ytimg.com/vi/xEuX6HCUFWI/maxresdefault.jpg";
+  final _imgUrl = "https://i.ytimg.com/vi/xEuX6HCUFWI/maxresdefault.jpg";
 
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Column(
-      children: [
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+        IntrinsicHeight(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: Container(
+                      color: Colors.red,
+                      child: Image.asset(
+                        "assets/images/ege.jpeg",
+                        fit: BoxFit.cover,
+                      )),
+                ),
+                Expanded(
+                  child: Container(
+                      color: Colors.red,
+                      child: Image.network(
+                        _imgUrl,
+                        fit: BoxFit.cover,
+                      )),
+                ),
+                Expanded(
+                  child: Container(
+                      child: CircleAvatar(
+                    backgroundImage: NetworkImage(_imgUrl),
+                  )),
+                ),
+              ],
+            ),
+          ),
         Container(
-            color: Colors.red,
-            width: 150,
-            height: 150,
-            child: Image.asset(
-              "assets/images/ege.jpeg",
-              fit: BoxFit.cover,
-            )),
-        Container(
-            color: Colors.red,
-            width: 150,
-            height: 150,
-            child: Image.network(
-              _imgUrl,
-              fit: BoxFit.cover,
-            )),
-        Container(
-            child: CircleAvatar(
-          backgroundImage: NetworkImage(_imgUrl),
-          radius: 75,
+          height: 200,
+          child: FadeInImage.assetNetwork(
+            fit: BoxFit.cover,
+            placeholder: "assets/images/loading.gif", 
+            image: _imgUrl
+          )
+        ),
+        Expanded(
+          child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Placeholder(color: Colors.red,),
         )),
       ],
     ));
